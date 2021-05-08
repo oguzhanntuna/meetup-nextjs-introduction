ceb14ae54fcd3523ed618c5fc138f9ab2ec775c1
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-05-08 21:00:47 +0300

    outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Istanbul_asv2020-02_img47_Galata_Tower.jpg',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Istanbul_asv2020-02_img47_Galata_Tower.jpg',
+        address: 'Some address 10, 12345 Some City',
+        description: 'This is a second meetup!'
+    }
+]
+
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage;
